c2aa862ff68f06a2efe7c1d54d8df32f2e33dbc5
norovdorj <norovdorj@gmail.com>
2021-09-29 22:45:50 -0500

    Added walmart

--- WalmartLogon/app/src/main/java/com/example/walmartlogon/MainActivity.kt
+++ WalmartLogon/app/src/main/java/com/example/walmartlogon/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.example.walmartlogon
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+    fun forgetPass(view: View) {
+        val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse("https://www.walmart.com/account/login"))
+        startActivity(browserIntent);
     }
 }
